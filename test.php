8c16133becc3413aa8b67ade47956febf30c9714
sanzokgyawali <sanjok.gyawali@itsutra.com>
2020-09-05 04:09:56 +0545

    Php basic

---
+++ test.php
@@ -0,0 +1,35 @@
+<?php
+$con=new mysqli('localhost','root','','db_phpclass');
+if(isset($_POST['submit']))
+{
+	$fname=$_POST['fname'];
+	$lname=$_POST['lname'];
+	$email=$_POST['email'];
+	$password=$_POST['password'];
+	$sql="insert into tbl_details(fname,lname,email,password)values('$fname','$lname','$email','$password')";
+    if(mysqli_query($con,$sql))
+    {
+    	echo "<script>alert('Inserted Successfully')</script>";
+    }
+    else
+    { 
+    	echo "<script>alert('Failed')</script>";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple Php</title>
+</head>
+<body>
+<form method="post">
+	First Name: <input type="text" name="fname" required><br><br>
+	Last Name: <input type="text" name="lname" required><br><br>
+	Email: <input type="email" name="email" required><br><br>
+	Password: <input type="password" name="password" required><br><br>
+	<input type="submit" name="submit" value="Submit">
+
+</form>
+</body>
+</html>
